f1386b0f011e05e38a0330d29935d867b00d316f
sbui056 <119729517+sbui056@users.noreply.github.com>
2025-03-15 18:29:22 -0700

    Initial Commit

---
+++ AirQualityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirQualityApp
+//
+//  Created by Steven Bui on 3/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AirQualityAppTests/AirQualityAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityAppTests.swift
+//  AirQualityAppTests
+//
+//  Created by Steven Bui on 3/15/25.
+//
+
+import Testing
+@testable import AirQualityApp
+
+struct AirQualityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
